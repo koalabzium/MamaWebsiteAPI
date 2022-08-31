deff006b910f09787991de86c01165fd230f6be0
koalabzium <zosia.owsiak@gmail.com>
2022-08-31 05:16:47 -0700

    Damian's updates

--- functions/utils/AuthUtils.js
+++ functions/utils/AuthUtils.js
@@ -1,26 +1,31 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
+const functions = require("firebase-functions");
 
-const {JWT_SECRET} = process.env;
+const getSecret = () => {
+  const config = functions.config();
+  const { secret } = config.mamalibrary;
+  return secret;
+};
 
 const verifyToken = (req, res, next) => {
-    const authHeader = req.headers["authorization"];
-    if (typeof authHeader == "undefined") res.sendStatus(403);
-    const token = authHeader.split(" ")[1];
-    jwt.verify(token, JWT_SECRET, (err, authData) => {
-        if (err) {
-            res.sendStatus(403);
-        } else {
-            req.authData = authData;
-            next();
-        }
-    });
+  const authHeader = req.headers["authorization"];
+  if (typeof authHeader == "undefined") res.sendStatus(403);
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, getSecret(), (err, authData) => {
+    if (err) {
+      res.sendStatus(403);
+    } else {
+      req.authData = authData;
+      next();
+    }
+  });
 };
 
 const signToken = (payload) => {
-    return jwt.sign(payload, JWT_SECRET);
+  return jwt.sign(payload, getSecret());
 };
 
 module.exports = {
-    verifyToken,
-    signToken
+  verifyToken,
+  signToken,
 };
